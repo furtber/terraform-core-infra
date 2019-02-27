20613fa9504e4205d62bfed75586a0113addcf97
Jukka Forsgren <forjukka@amazon.com>
2019-02-27 22:33:05 +0100

    Fix output references.

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,7 @@
 # VPC
 output "vpc_id"   { value = "${module.vpc.vpc_id}" }
-output "vpc_cidr" { value = "${module.vpc.vpc_cidr}" }
+output "vpc_cidr" { value = "${module.vpc.vpc_cidr_block}" }
 
 # subnets
-output "public_subnet_ids"  { value = "${module.public_subnets.subnet_ids}" }
-output "private_subnets_t1_ids" { value = "${module.private_subnets_t1.subnet_ids}" }
-output "private_subnets_t2_ids" { value = "${module.private_subnets_t2.subnet_ids}" }
+output "public_subnet_ids"  { value = "${module.vpc.public_subnets}" }
+output "private_subnet_ids" { value = "${module.vpc.private_subnets}" }
