7e095b7ad8087283e4f7ea73cb84154d24ae58fa
Jukka Forsgren <forjukka@amazon.com>
2019-02-27 20:53:12 +0100

    Move some attributes to terraform init.

--- backend.tf
+++ backend.tf
@@ -1,9 +1,7 @@
 terraform {
         backend "s3" {
                 dynamodb_table = "TerraformStateLock"
-                ###bucket = DEFINED IN TERRAFORM INIT
-                key = "core-infra"
-                region = "${AWS_REGION}"
+                region = "eu-west-1"
         }
 }
 
