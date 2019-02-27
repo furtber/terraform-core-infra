08ff7dd89c29899531296df0da53bcfe8409cb9e
Jukka Forsgren <forjukka@amazon.com>
2019-02-27 22:37:55 +0100

    Variables set in environment specific tfvars file must be defined here.

--- inputs.tf
+++ inputs.tf
@@ -1,4 +1,23 @@
-# Terraform variable set from shell environment
-# Accessible in terraform with TF_VAR_ENVIRONMENT
+#
+# This variable is passed from Jenkins parameter
+#
 variable "ENVIRONMENT" {}
 
+#
+# These variables are defined in ENVIRONMENT.tfvars
+#
+variable "vpc_name"         {
+  description = "Name of the VPC."
+}
+variable "vpc_cidr"         {
+  description = "VPC address range in CIDR format."
+}
+variable "azs"              {
+  description = "Availability zones that are used."
+}
+variable "public_subnets"   {
+  description = "List of public subnets."
+}
+variable "private_subnets"  {
+  description = "List of private subnets."
+}
