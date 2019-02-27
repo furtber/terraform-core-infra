56505e2161173c70712f577a68ddfd17fe7d969e
Jukka Forsgren <forjukka@amazon.com>
2019-02-27 11:09:18 +0100

    Made minor modifications to the bootstrap script.

--- bootstrap/create-terraform-state-bucket.sh
+++ bootstrap/create-terraform-state-bucket.sh
@@ -4,24 +4,31 @@
 # S3 bucket for storing state and DynamoDB table for locking are created.
 #
 
-if [[ -z "$APP_ENVIRONMENT" ]]; then
-  echo "Error: Please set TF_VAR_APP_ENVIRONMENT variable"
+if [[ -z "$AWS_DEFAULT_PROFILE" ]]; then
+  echo "Error: Please set AWS_DEFAULT_PROFILE variable to select which AWS account to set up."
   exit 1
 fi
 
-S3_BUCKET="$APP_ENVIRONMENT-terraform-tfstate-$AWS_ACCOUNT_ID"
+if [[ -z "$ENVIRONMENT" ]]; then
+  echo "Error: Please set ENVIRONMENT variable to have that included in the S3 bucket name. E.g. DEV, STAGE, PROD"
+  exit 1
+fi
+
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
+
+S3_BUCKET="$ENVIRONMENT-terraform-tfstate-$AWS_ACCOUNT_ID"
 
 echo "Creating S3 bucket: $S3_BUCKET"
 aws s3api create-bucket --bucket $S3_BUCKET --create-bucket-configuration LocationConstraint="eu-west-1"
 
 echo "Waiting until the bucket is ready..."
-aws s3api wait bucket-exists --bucket $TF_VAR_S3_BUCKET
+aws s3api wait bucket-exists --bucket $S3_BUCKET
 
 echo "Enabling versioning for the bucket"
-aws s3api put-bucket-versioning --bucket $TF_VAR_S3_BUCKET --versioning-configuration Status=Enabled
+aws s3api put-bucket-versioning --bucket $S3_BUCKET --versioning-configuration Status=Enabled
 
 echo "Putting tags to bucket"
-aws s3api put-bucket-tagging --bucket $TF_VAR_S3_BUCKET --tagging "TagSet=[{Key=environment,Value=$TF_VAR_APP_ENVIRONMENT},{Key=cost_center,Value=123},{Key=service,Value=testing},{Key=business_unit,Value=testing},{Key=role,Value=Terraform}]"
+aws s3api put-bucket-tagging --bucket $S3_BUCKET --tagging "TagSet=[{Key=environment,Value=$ENVIRONMENT},{Key=cost_center,Value=123},{Key=service,Value=foo},{Key=business_unit,Value=foo},{Key=role,Value=Terraform}]"
 
 echo "Put S3 bucket policy"
 TIMESTAMP=$(date +%Y%m%d%H%M)
@@ -55,8 +62,8 @@ POLICY=$(cat<<EOF
 }
 EOF
 )
-aws s3api put-bucket-policy --bucket $TF_VAR_S3_BUCKET --policy "$POLICY"
+aws s3api put-bucket-policy --bucket $S3_BUCKET --policy "$POLICY"
 
 echo "Creating prefix: core-infra"
-aws s3api put-object --bucket $TF_VAR_S3_BUCKET --key core-infra/ --server-side-encryption AES256
+aws s3api put-object --bucket $S3_BUCKET --key core-infra/ --server-side-encryption AES256
 
