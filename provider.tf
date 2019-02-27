1b2c04a3420fe3dd39919779972b2162d6c7c874
Jukka Forsgren <forjukka@amazon.com>
2019-02-27 21:03:11 +0100

    Use terraform workspaces as instructed in aws provider docs.

--- provider.tf
+++ provider.tf
@@ -1,3 +1,16 @@
-provider "aws" {
-  region = "eu-west-1"
+variable "workspace_iam_roles" {
+  default = {
+    staging = "arn:aws:iam::200562504897:role/Jenkins"
+    production = "arn:aws:iam::980331777790:role/Jenkins"
+  }
 }
+
+provider "aws" {
+  # No credentials explicitly set here because they come from either the
+  # environment or the global credentials file.
+
+  assume_role = "${var.workspace_iam_roles[terraform.workspace]}"
+  region = eu-west-1
+}
+
+
