a5a861d5042ae4262a1dc8ecb89d7c45f949a483
Jukka Forsgren <forjukka@amazon.com>
2019-02-27 20:00:23 +0100

    Move away from deprecated attribute.

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
         backend "s3" {
-                lock_table = "TerraformStateLock"
+                dynamodb_table = "TerraformStateLock"
                 ###bucket = DEFINED IN TERRAFORM INIT
                 key = "core-infra"
                 region = "${AWS_REGION}"
