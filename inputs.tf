d60044752a1ea6a39312caf7021106e771a22acf
Jukka Forsgren <forjukka@amazon.com>
2019-02-28 05:53:38 +0100

    Improve formatting.

--- inputs.tf
+++ inputs.tf
@@ -6,21 +6,21 @@ variable "ENVIRONMENT" {}
 #
 # These variables are defined in ENVIRONMENT.tfvars
 #
-variable "vpc_name"         {
+variable "vpc_name" {
   description = "Name of the VPC."
 }
-variable "vpc_cidr"         {
+variable "vpc_cidr" {
   description = "VPC address range in CIDR format."
 }
-variable "azs"              {
+variable "azs" {
   type = list
   description = "Availability zones that are used."
 }
-variable "public_subnets"   {
+variable "public_subnets" {
   type = list
   description = "List of public subnets."
 }
-variable "private_subnets"  {
+variable "private_subnets" {
   type = list
   description = "List of private subnets."
 }
